a04c4488c0383ea0674acbeaee89ce40fbcf7a2b
truongan <antran070402@gmail.com>
2024-06-21 15:30:50 +0700

    init project

---
+++ src/advance/dev/MainApp.java
@@ -0,0 +1,13 @@
+package advance.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person p = new Person("Nam",23);
+		System.out.println(p.Name);
+		System.out.println(p.age);
+		
+	}
+
+}
---
+++ src/advance/dev/Person.java
@@ -0,0 +1,11 @@
+package advance.dev;
+
+public class Person {
+	String Name;
+	int age;
+	public Person(String Name, int age){
+		this.Name = Name;
+		this.age = age;
+	}
+
+}
